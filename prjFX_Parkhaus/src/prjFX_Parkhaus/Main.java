5ceb5b3a473c6075325829cc7a4c091ed5ddaf0a
Tabbbi <tabea@laschen.com>
2023-09-29 22:54:50 +0200

    Exceptions

--- prjFX_Parkhaus/src/prjFX_Parkhaus/Main.java
+++ prjFX_Parkhaus/src/prjFX_Parkhaus/Main.java
@@ -1,14 +1,12 @@
 package prjFX_Parkhaus;
 
 public class Main {
-	
 	public static void main(String[] args) {
 		Parkhaus p1 = new Parkhaus(100, 5);
-		p1.addCar(5);
 		p1.addCar(6);
-		p1.addCars(75);
+		p1.removeCar(6);
+		p1.removeCar(6);
 		p1.trafficlight();
 		p1.show();
-
 	}
 }
